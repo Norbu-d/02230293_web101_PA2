b3e664453553f4de99f6828589a83f9e490360a7
Norbu-d <02230293.cst@rub.edu.bt>
2024-09-09 17:39:34 +0600

    dw

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // You can specify any port number you want for local development
+    host: '0.0.0.0' // Allows external access (e.g., from other devices on the same network)
+  }
+});
